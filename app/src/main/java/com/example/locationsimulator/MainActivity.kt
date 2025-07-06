292f2ba0c9135c95676e4e8b05a65b58a40eb113
Your Name <your_email@example.com>
2025-07-06 21:35:47 +0800

    Add settings.gradle and simplify app for initial build

--- app/src/main/java/com/example/locationsimulator/MainActivity.kt
+++ app/src/main/java/com/example/locationsimulator/MainActivity.kt
@@ -9,38 +9,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.locationsimulator.ui.theme.LocationSimulatorTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LocationSimulatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Text("Hello Android!")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationSimulatorTheme {
-        Greeting("Android")
     }
 }
